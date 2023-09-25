9acc0b0654fcfa564e2e48913aa3982f622098a6
Miexie R2 Network <sephjavier@gmail.com>
2023-09-25 11:51:59 +0800

    update footer links

--- components/includes/Footer.jsx
+++ components/includes/Footer.jsx
@@ -8,6 +8,31 @@ const Footer = ({domain}) => {
           <div className="col-xl-12 text-center">
             Copyright {new Date().getFullYear()}. {domain[0].toUpperCase() + domain.substring(1)}
           </div>
+          <div className="mt-1 flex justify-center gap-1 text-center text-sm text-gray-500 dark:text-gray-600">
+        <span>
+          <a href={`https://domaindirectory.com/servicepage/?domain=${domain}`} rel="noopener" target="_blank">
+            Build
+          </a>
+        </span>{' '}
+        <span>·</span>
+        <span>{' '}
+          <a href={`https://domaindirectory.com/servicepage/?domain=${domain}`} rel="noopener" target="_blank">
+            Invest
+          </a>
+        </span>{' '}
+        <span>·</span>
+        <span>{' '}
+          <a href={`https://domaindirectory.com/servicepage/?domain=${domain}`} rel="noopener" target="_blank">
+            Manage
+          </a>
+        </span>
+        <span>·</span>
+        <span>{' '}
+          <a href={`https://domaindirectory.com/servicepage/?domain=${domain}`} rel="noopener" target="_blank">
+            Monetize 
+          </a>
+        </span>
+      </div>
         </div>
       </div>
     </footer>
