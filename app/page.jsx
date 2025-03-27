04b05384523923f388357b0459cebc84d07b0b12
jaysoncano10 <cano.jayson10@gmail.com>
2025-03-27 15:03:40 +0800

    add top header ticker and adao section

--- app/page.jsx
+++ app/page.jsx
@@ -1,18 +1,19 @@
+import BannerSection from "@/components/BannerSection";
+import Footer from "@/components/includes/Footer";
 import HeaderWidget from "@/components/includes/HeaderWidget";
 import Navigation from "@/components/includes/Navigation";
-import Footer from "@/components/includes/Footer";
 import TabSection from "@/components/includes/TabSection";
 import TopChallengeSites from "@/components/TopChallengeSites";
-import BannerSection from "@/components/BannerSection";
+import BlogList from "@/modules/blog/BlogList";
 import { BsArrowRightCircle } from "react-icons/bs";
 import {
-  getDomain,
-  getTopChallengeSites,
-  getSponsorDetails,
-  getLatestChallenges,
   getData,
+  getDomain,
+  getLatestChallenges,
+  getSponsorDetails,
+  getTopChallengeSites
 } from "../lib/data";
-import BlogList from "@/modules/blog/BlogList";
+import CtaAdaoSection from "./home/CtaAdaoSection";
 
 export default async function Home() {
   const c = await getData();
@@ -22,31 +23,55 @@ export default async function Home() {
   const latest_challenge = await getLatestChallenges();
 
   const {
-    data: { sponsor_details },
+    data: { sponsor_details }
   } = sponsor_count;
   const { count, amount } = sponsor_details[0];
 
   return (
     <>
+      <section className="tw-w-full tw-bg-[#1A122A] tw-text-white tw-fixed tw-top-0 tw-z-50">
+        <div className="container tw-overflow-hidden tw-items-center tw-flex">
+          <marquee
+          
+            direction="left"
+            height="33px"
+            className="tw-text-sm tw-flex tw-gap-2 tw-items-center"
+          >
+            <span className="tw-inline-flex tw-text-[#00f6ff] tw-mr-2">
+              Flash News!
+            </span>
+            <a
+              target="_blank"
+              rel="noopener noreferrer"
+              class="tw-text-white/90 hover:tw-text-white tw-transition-colors tw-inline-flex tw-items-center blink"
+              href="https://adao.ai/?referral=jstack.com"
+            >
+              ADAO token is dropping to your Base chain soon! Get ADAO today
+              while it&apos;s on sale!
+            </a>
+          </marquee>
+        </div>
+      </section>
       <HeaderWidget
         domain={domain}
         piwikId={c.data.piwikId}
         accountGA={c.data.accountGA}
         adsenseClientId={c.data.adsenseClientId}
       />
-      <section className="header-bg xl:tw-min-h-screen tw-bg-cover tw-bg-bottom tw-relative tw-text-white tw-pb-24">
+      <section className="header-bg  tw-w-full tw-mt-[78px] tw-bg-cover tw-bg-bottom tw-relative tw-text-white tw-pb-24">
         <Navigation />
         <BannerSection />
       </section>
-      <section
-        className="tw-py-24 tw-bg-white"
-        id="play-section"
-      >
+      <CtaAdaoSection />
+      <section className="tw-py-24 tw-bg-white" id="play-section">
         <div className="container">
           <div className="row">
             <div className="col-xl-12">
               <h1 className="text-center tw-text-5xl mb-5 tw-font-bold">
-                Play With <span className="tw-relative text-ripple tw-font-extrabold">511</span>{" "}
+                Play With{" "}
+                <span className="tw-relative text-ripple tw-font-extrabold">
+                  511
+                </span>{" "}
                 Players
               </h1>
             </div>
@@ -56,7 +81,9 @@ export default async function Home() {
           </div>
           <div className="row tw-justify-center">
             <div className="col-xl-10">
-              <TopChallengeSites top_challenge_domains={top_challenge_domains} />
+              <TopChallengeSites
+                top_challenge_domains={top_challenge_domains}
+              />
             </div>
           </div>
         </div>
@@ -74,39 +101,33 @@ export default async function Home() {
               </div>
               <div className="col-xl-12 tw-text-center">
                 <h2 className="tw-font-bold">
-                  <span className="tw-font-extrabold">{count}</span> Challenges Sponsored Amounting
-                  To <span className="tw-font-extrabold">${amount}</span>
+                  <span className="tw-font-extrabold">{count}</span> Challenges
+                  Sponsored Amounting To{" "}
+                  <span className="tw-font-extrabold">${amount}</span>
                 </h2>
               </div>
             </div>
           </div>
         </div>
       </section>
-      <section
-        className="tw-py-24 lg:tw-pt-0 tw-bg-white"
-        id="sponsor-section"
-      >
+      <section className="tw-py-24 lg:tw-pt-0 tw-bg-white" id="sponsor-section">
         <div className="container">
           <div className="row">
             <div className="col-xl-12">
               <div className="challenge-section-tabs">
-                <TabSection
-                  challenges={latest_challenge}
-                  domain={domain}
-                />
+                <TabSection challenges={latest_challenge} domain={domain} />
               </div>
             </div>
           </div>
         </div>
       </section>
-      <section
-        className="tw-py-24 tw-bg-[#fafafa]"
-        id="earn-section"
-      >
+      <section className="tw-py-24 tw-bg-[#fafafa]" id="earn-section">
         <div className="container">
           <div className="row">
             <div className="col-xl-12">
-              <h2 className="mb-5 text-center tw-font-semibold">3 Ways To Earn Tokens</h2>
+              <h2 className="mb-5 text-center tw-font-semibold">
+                3 Ways To Earn Tokens
+              </h2>
             </div>
             <div className="col-xl-12">
               <div className="row justify-content-center text-center">
